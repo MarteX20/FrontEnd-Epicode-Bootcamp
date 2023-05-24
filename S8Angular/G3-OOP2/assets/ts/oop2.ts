b8bd3c574e61175bbfc90ccd612e31a9d853017f
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-05-24 17:38:23 +0200

    .

--- S8Angular/G3-OOP2/assets/ts/oop2.ts
+++ S8Angular/G3-OOP2/assets/ts/oop2.ts
@@ -1,4 +1,4 @@
-export class App {
+class App {
     codredd: number;
     ral: number;
     tasseinps: number;
@@ -24,3 +24,21 @@ export class App {
         return this.ral - this.getTasselnps() - this.getTasselrpef();
     }
 }
+
+class Persona extends App {
+    constructor(_codredd: number, _ral: number) {
+        super(_codredd, _ral);
+    }
+}
+
+const Lav1 = new Persona(20000, 0.67)
+
+console.log('Lavoratore 1 guadagna al netto', Lav1.getRedditoAnnuoNetto());
+
+const Lav2 = new Persona(25000, 0.67)
+
+console.log('Lavoratore 2 guadagna al netto', Lav2.getRedditoAnnuoNetto());
+
+const Lav3 = new Persona(30000, 0.67)
+
+console.log('Lavoratore 3 guadagna al netto', Lav3.getRedditoAnnuoNetto());
--- S8Angular/G3-OOP2/assets/ts/oop2sub.ts
+++ S8Angular/G3-OOP2/assets/ts/oop2sub.ts
@@ -1,11 +1,3 @@
-import { App } from "./oop2";
+// import { App } from "./oop2";
 
-class Persona extends App {
-    constructor(_codredd: number, _ral:number) {
-            super(_codredd, _ral);
-        }
-}
 
-const Lav1 = new Persona(0.67, 30000)
-
-console.log(Lav1);
