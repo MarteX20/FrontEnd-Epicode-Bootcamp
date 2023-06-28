fd0cdb1d8dbe76502d8cc9fdaf7c36fbb655d266
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-06-28 15:59:51 +0200

    implementato lo splitting in esrcizio 3

--- Backend/S1G3/src/Es3/Main.java
+++ Backend/S1G3/src/Es3/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		StringBuilder result = new StringBuilder();
 
 		System.out.println("Inserire stringa (digitare \":q\" per terminare): ");
 		String input = scanner.nextLine();
 
 		while (!input.equals(":q")) {
-			// System.out.println(input);
+			for (int i = 0; i < input.length(); i++) {
+				char c = input.charAt(i);
+				result.append(c).append(",");
+			}
+
+
 			System.out.println("Inserire stringa (digitare \":q\" per terminare): ");
 			input = scanner.nextLine();
-			System.out.println("Stringa inserita: " + input);
+			System.out.println("Stringa inserita: " + result.toString());
 		}
 
 		System.out.println("Programma terminato!");
