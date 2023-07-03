2e111fc4bad48891e984086706654e1b012a7ce2
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-07-03 16:24:01 +0200

    Sostituito il do while con while normale finito Es2

--- Backend/S2G1/src/Es2/Main.java
+++ Backend/S2G1/src/Es2/Main.java
@@ -9,33 +9,28 @@ public class Main {
         float km;
         float litri;
 
-        do {
+        while (true) {
             try {
-                System.out.println("Inserire i km percorsi (Per chiudere inserire -1): ");
+                System.out.println("Inserire i km percorsi (Per terminare inserire 0): ");
                 km = scanner.nextFloat();
 
-                System.out.println("Inserire i litri di carburante consumati (Per chiudere proseguire avanti): ");
+                if (km == 0)
+                    break;
+
+                System.out.println("Inserire i litri di carburante consumati: ");
                 litri = scanner.nextFloat();
 
-                calcolo(litri, km);
-
-                if (litri == 0 && km == 0) {
-                    System.out.println("Ops, non ti sei prorpio spostato da nessuna parte ;) (Non si divide per lo zero)");
+                if (litri == 0) {
                     throw new ArithmeticException("Divisione per zero!");
                 }
 
-                if (km == -1)
-                    break;
+                float consumo = km / litri;
+                System.out.println("Hai percorso " + consumo + " km con 1 litro di carburante");
             } catch (ArithmeticException e) {
                 System.out.println("Errore: " + e.getMessage());
             }
-        } while (true);
+        }
 
-        System.out.println("Calcolo finito");
-    }
-
-    public static void calcolo(float litri, float km){
-        float consumo = litri / km;
-        System.out.println("Hai consumato " + consumo + " litri facendo " + km);
+        System.out.println("Programma terminato");
     }
 }
